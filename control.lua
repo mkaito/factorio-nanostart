0f9251db7b3ec9a26b7104357895f8f7b909ed1f
Michishige Kaito <me@mkaito.com>
2018-03-17 17:50:06 +0000

    Initial version

---
+++ control.lua
@@ -0,0 +1,42 @@
+script.on_event(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+  local guns = player.get_inventory(defines.inventory.player_guns)
+  local ammo = player.get_inventory(defines.inventory.player_ammo)
+
+  local equip = settings.get_player_settings(game.players[event.player_index])["nanostart-equip"].value
+  local emitters = settings.get_player_settings(game.players[event.player_index])["nanostart-emitters"].value
+  local constructor_stacks = settings.get_player_settings(game.players[event.player_index])["nanostart-constructor-stacks"].value
+  local termite_stacks = settings.get_player_settings(game.players[event.player_index])["nanostart-termite-stacks"].value
+
+  local constructors = constructor_stacks * 100
+  local termites = termite_stacks * 100
+
+  -- Check for sandbox mode... or any mode without a player body.
+  if (not player.character or not player.get_inventory(5)) then
+    return
+  end
+
+  if equip then
+    if constructors > 100 and termites > 100 then
+      if emitters >= 1 then
+        guns.insert("gun-nano-emitter")
+      end
+
+      player.insert{ name="ammo-nano-constructors", count = constructors }
+
+      if emitters >= 2 then
+        guns.insert("gun-nano-emitter")
+      end
+
+      player.insert{ name="ammo-nano-termites", count = termites }
+
+      if emitters >= 3 then
+        player.insert{ name = "gun-nano-emitter", count = (emitters - 2) }
+      end
+    end
+  else
+    player.insert{ name="ammo-nano-termites", count = termites }
+    player.insert{ name="ammo-nano-constructors", count = constructors }
+    player.insert{ name="gun-nano-emitter", count = emitters }
+  end
+end)
---
+++ settings.lua
@@ -0,0 +1,26 @@
+data:extend({
+    {
+      type = "bool-setting",
+      name = "nanostart-equip",
+      setting_type = "runtime-per-user",
+      default_value = true
+    },
+    {
+      type = "int-setting",
+      name = "nanostart-emitters",
+      setting_type = "runtime-per-user",
+      default_value = 1
+    },
+    {
+      type = "int-setting",
+      name = "nanostart-constructor-stacks",
+      setting_type = "runtime-per-user",
+      default_value = 3
+    },
+    {
+      type = "int-setting",
+      name = "nanostart-termite-stacks",
+      setting_type = "runtime-per-user",
+      default_value = 2
+    }
+})
